83971b6b1cc2f6d930607e6d21b423b1edcb73f2
vaishnavinambiar <vaishnaviknambiar1996@gmail.com>
2018-12-24 14:16:17 +0530

    Battleship testcase

---
+++ battleship_tc.py
@@ -0,0 +1,19 @@
+import unittest
+from task_battleship import print_board
+from task_battleship import random_row
+from task_battleship import random_col
+
+class MyTest(unittest.TestCase):
+   def test_print_board(self):
+     
+       self.assertTrue(True)
+       
+   def test2_random_row(self):
+       self.assertTrue(True)
+
+   def test3_random_col(self):
+        self.assertFalse(False)
+   
+
+if __name__== '__main__':
+    unittest.main()
